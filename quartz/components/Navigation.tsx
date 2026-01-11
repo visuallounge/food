a9f91c439b3a5559b92c8cf522cb4ebe28e06266
Tina Mueller <tinamueller@Mac.fritz.box>
2026-01-11 10:49:16 +0100

    Fix navigation links for GitHub Pages subdirectory

--- quartz/components/Navigation.tsx
+++ quartz/components/Navigation.tsx
@@ -1,21 +1,23 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { pathToRoot } from "../util/path"
 
 const categories = [
-  { name: "Eis", path: "/Eis/" },
-  { name: "Hauptgerichte", path: "/Hauptgerichte/" },
-  { name: "Vorspeisen", path: "/Vorspeisen/" },
-  { name: "Suppen", path: "/Suppen/" },
-  { name: "Kids", path: "/Kids/" },
-  { name: "Salate", path: "/Salate/" },
-  { name: "Desserts", path: "/Desserts/" },
+  { name: "Eis", path: "Eis" },
+  { name: "Hauptgerichte", path: "Hauptgerichte" },
+  { name: "Vorspeisen", path: "Vorspeisen" },
+  { name: "Suppen", path: "Suppen" },
+  { name: "Kids", path: "Kids" },
+  { name: "Salate", path: "Salate" },
+  { name: "Desserts", path: "Desserts" },
 ]
 
 export default (() => {
-  const Navigation: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
+  const Navigation: QuartzComponent = ({ displayClass, fileData }: QuartzComponentProps) => {
+    const baseDir = pathToRoot(fileData.slug!)
     return (
       <nav class={`nav-menu ${displayClass ?? ""}`}>
         {categories.map(({ name, path }) => (
-          <a href={path}>{name}</a>
+          <a href={`${baseDir}/${path}/`}>{name}</a>
         ))}
       </nav>
     )
